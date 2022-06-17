b1fe72c70bb1a0d7d24dcd257b25864e354c508f
Tamiris Shigaki <shigakiendo@gmail.com>
2022-06-17 18:27:24 -0300

    :sparkles: feat: requisito 1 - Crie o endpoint GET /talker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -10,6 +11,21 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+function getTalker() {
+  return fs.readFile('./talker.json', 'utf-8')
+    .then((fileContent) => JSON.parse(fileContent));
+}
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const talkers = await getTalker();
+
+    return res.status(HTTP_OK_STATUS).json(talkers);
+  } catch (error) {
+    return res.status(HTTP_OK_STATUS).end();
+  }
 });
 
 app.listen(PORT, () => {
