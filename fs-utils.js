5691cbf7468d02f96d6eb3bc5684904801b837fc
Tamiris Shigaki <shigakiendo@gmail.com>
2022-06-21 11:08:10 -0300

    ci: Modificaçao no requisito 4

--- fs-utils.js
+++ fs-utils.js
@@ -3,6 +3,6 @@ const fs = require('fs/promises');
 function getTalker() {
     return fs.readFile('./talker.json', 'utf-8')
       .then((fileContent) => JSON.parse(fileContent));
-}
+  }
 
 module.exports = { getTalker };
--- middlewares/validateEmail.js
+++ middlewares/validateEmail.js
@@ -1,19 +1,17 @@
 module.exports = (req, res, next) => {
-    const { email } = req.body;
-  
-    if (!email) {
+  const { email } = req.body;
+
+  if (!email) {
       return res.status(400).json({
-        message: 'O campo "email" é obrigatório',
-      });
-    }
-  
-    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-  
-    if (!emailRegex.test(email)) {
+          message: 'O campo "email" é obrigatório',
+        });
+  }
+
+  const emailRegex = /\S+@\S+\.\S+/;
+  if (!emailRegex.test(email)) {
       return res.status(400).json({
-        message: 'O "email" deve ter o formato "email@email.com"',
-      });
-    }
-  
-    next();
-  };
+          message: 'O "email" deve ter o formato "email@email.com"',
+        });
+  }
+  next();
+};
